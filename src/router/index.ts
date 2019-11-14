918e51ab762707fbb42350ca14e401aee3d76d53
yangwei <yangwei@authine.com>
2019-11-14 17:56:58 +0800

    项目初始化

---
+++ src/router/index.ts
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import MyInstance from '../views/processCenter/MyInstance.vue'
+import MyUnfinishedWorkItem from '../views/processCenter/MyUnfinishedWorkItem.vue'
+import MyWorkflow from '../views/processCenter/MyWorkflow.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'home',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue'),
+    children:[
+      {
+       
+        path: 'MyInstance',
+        component: MyInstance
+      },
+      {
+       
+        path: 'MyUnfinishedWorkItem',
+        component: MyUnfinishedWorkItem
+      },
+      {
+       
+        path: 'MyWorkflow',
+        component: MyWorkflow
+      },
+      
+    ]
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import('../views/Login.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
